7b4634da8c9ad5ad012857585b0ca8cb6e7a3a6f
Kate Jewett-Williams <kjewettwilliams@gmail.com>
2019-02-13 05:13:07 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,10 +9,10 @@ class PostsController < ApplicationController
 
   def update
     if @post.update!(post_params)
-      render :edit
-    else
-      @post.update(post_params)
       redirect_to post_path(@post)
+    else
+      
+      render :edit
     end
   end
 
